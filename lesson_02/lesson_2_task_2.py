a51bf1d87bcf321b55145d5be0c96683f873ab02
xtrimi8 <xtrimi@rambler.ru>
2026-08-13 20:25:31 +0300

    Исправлены все замечания flake8 и наставника

--- lesson_02/lesson_2_task_2.py
+++ lesson_02/lesson_2_task_2.py
@@ -10,16 +10,6 @@ result = is_year_leap(year)
 print("год", year, ":", result)
 
 
-def is_year_leap(year):
-    if year % 4 == 0:
-        return True
-    else:
-        return False
-
 year = 2023
-
-
 result = is_year_leap(year)
-
-
 print("год", year, ":", result)
